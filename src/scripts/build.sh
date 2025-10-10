deacba4955cb84ca527ecdedd6bfa3f12f32d8ce
Heinz-Günter Hermanns <hgh.dev@t-online.de>
2025-10-10 07:02:39 +0000

    scripts

--- src/scripts/build.sh
+++ src/scripts/build.sh
@@ -2,5 +2,8 @@
 
 set -ex
 
-version=0.138.3.53
-./build_release.sh -a arm64 -l linux -b ${version}
+# https://github.com/bmx-ng/bmx-ng/releases?q=BlitzMax+RaspberryPi+arm64
+# version=0.138.3.53
+version=0.150.3.58
+timestamp=202510050425
+./build_release.sh -a arm64 -l linux -b ${version} -t ${timestamp}
